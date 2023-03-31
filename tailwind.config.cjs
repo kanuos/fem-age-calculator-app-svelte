73b3f3ca70baa553b1b9fef033d23338f1854cd6
Sounak <sounak.theone@gmail.com>
2023-03-31 11:32:31 -0400

    Initial commit - install svelte - install tailwind, postcss, autoprefixer - added google font - customized tailwind - added assets, favicon

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,19 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,svelte}"],
   theme: {
     extend: {},
+    colors: {
+      "primary-1": "hsl(259, 100%, 65%)",
+      "primary-2": "hsl(0, 100%, 67%)",
+
+      "neutral-1": "hsl(0, 0%, 100%)",
+      "neutral-2": "hsl(0, 0%, 94%)",
+      "neutral-3": "hsl(0, 0%, 86%)",
+      "neutral-4": "hsl(0, 1%, 44%)",
+      "neutral-5": "hsl(0, 0%, 8%)",
+    },
+    fontFamily: {
+      sans: "Poppins",
+    },
   },
   plugins: [],
 };
